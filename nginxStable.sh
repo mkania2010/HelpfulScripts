3478e598820f91f015473aaf8bb61862cd1b9412
mkania2010 <mkania2010+github@gmail.com>
2020-10-26 12:58:15 -0600

    changed formatting

--- nginxStable.sh
+++ nginxStable.sh
@@ -6,8 +6,7 @@ echo "Installing curl gnupg2 ca-certificates lsb-release"
 sudo apt install curl gnupg2 ca-certificates lsb-release
 
 echo "adding the apt repo for nginx"
-echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
-    | sudo tee /etc/apt/sources.list.d/nginx.list
+echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
 
 echo "adding the nginx signing key"
 curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
